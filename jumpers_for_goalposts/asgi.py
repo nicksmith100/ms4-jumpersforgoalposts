35fcf872809b9925f332a00355b5eacc2867e3c1
Nick Smith <nicksmith100@yahoo.com>
2024-01-24 23:17:11 +0000

    Install Django and create project

---
+++ jumpers_for_goalposts/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for jumpers_for_goalposts project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jumpers_for_goalposts.settings')
+
+application = get_asgi_application()
---
+++ jumpers_for_goalposts/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jumpers_for_goalposts project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jumpers_for_goalposts.settings')
+
+application = get_wsgi_application()
