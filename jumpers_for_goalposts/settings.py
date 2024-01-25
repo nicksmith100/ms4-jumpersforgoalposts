51b62cd09444252796ce713e55906e92ad75f62c
Nick Smith <nicksmith100@yahoo.com>
2024-01-25 21:53:28 +0000

    Remove insecure SECRET_KEY

--- jumpers_for_goalposts/settings.py
+++ jumpers_for_goalposts/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'django-insecure-asx#hixwn+s#q$d$i0ijckn=^gq-r=j534g=qum9ca331kezf1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
