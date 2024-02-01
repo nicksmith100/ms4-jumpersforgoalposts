51fe01643ce23908b6f1878c7141e0ef7c436a50
Nick Smith <nicksmith100@yahoo.com>
2024-02-01 22:56:41 +0000

    Customised admin

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,38 @@ from django.contrib import admin
 from .models import League, Team, Condition, Product
 
 # Register your models here.
-admin.site.register(League)
-admin.site.register(Team)
-admin.site.register(Condition)
-admin.site.register(Product)
+class LeagueAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class ConditionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'team',
+        'year',
+        'condition',
+        'price',
+        'image',
+    )
+
+    ordering = ('year', 'team',)
+
+admin.site.register(League, LeagueAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Condition, ConditionAdmin)
+admin.site.register(Product, ProductAdmin)
