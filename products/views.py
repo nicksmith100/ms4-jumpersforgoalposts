2e0b92c7f8a8a0c3ab1f9cdf7b1154f79f63601e
Nick Smith <nicksmith100@yahoo.com>
2024-02-02 00:20:03 +0000

    Add product images, details to products template

--- products/views.py
+++ products/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def all_products(request):
     """ View to return all products, including sorting and search queries """
+    
+    products = Product.objects.all()
 
-    return render (request, 'products/products.html')
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
