5d4fc508dcf76d6c1d1be5c7e03ebad237b31058
Nick Smith <nicksmith100@yahoo.com>
2024-02-02 23:29:48 +0000

    Add filtering by league, year and specials

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, League
 
 # Create your views here.
 
@@ -10,8 +10,28 @@ def all_products(request):
     
     products = Product.objects.all()
     query = None
+    leagues = None
+    years = None
+    player_issue = False
+    signed = False
 
     if request.GET:
+        if 'league' in request.GET:
+            leagues = request.GET['league'].split(",")
+            products = products.filter(league__name__in=leagues)
+            leagues = League.objects.filter(name__in=leagues)
+        
+        if 'year' in request.GET:
+            years = request.GET['year'].split(",")
+            products = products.filter(year__in=years)
+            years = Product.objects.filter(year__in=years)
+
+        if 'player_issue' in request.GET:
+            products = products.filter(player_issue=True)
+
+        if 'signed' in request.GET:
+            products = products.filter(signed=True)
+        
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,6 +44,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_leagues': leagues,
     }
 
     return render(request, 'products/products.html', context)
