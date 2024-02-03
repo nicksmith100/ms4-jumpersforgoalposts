6ad95595a4b5669283b6e620a77d9a76b753c1d3
Nick Smith <nicksmith100@yahoo.com>
2024-02-03 20:21:47 +0000

    Add modal for full-size product images

--- static/js/script.js
+++ static/js/script.js
@@ -8,4 +8,24 @@ $(document).ready(function(){
         } else { this.classList.toggle("flipped"); }
     });
 
+
+    var imageModal = document.getElementById('image-modal')
+    imageModal.addEventListener('show.bs.modal', function (event) {
+    // Anchor that triggered the modal
+    var anchor = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    var image = anchor.getAttribute('data-bs-image')
+    var imageTitle = anchor.getAttribute('data-bs-image-title')
+    // If necessary, you could initiate an AJAX request here
+    // and then do the updating in a callback.
+    //
+    // Update the modal's content.
+    var modalTitle = imageModal.querySelector('.modal-title')
+    var modalBody = imageModal.querySelector('.modal-body')
+
+    modalTitle.textContent = imageTitle
+    modalBody.innerHTML = `<img src="${image}" alt="${imageTitle}" class="w-100">`
+    })
+
+
 });
