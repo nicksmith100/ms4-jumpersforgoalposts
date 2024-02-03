a1cfe09221bf779ee809737207154e515dce8180
Nick Smith <nicksmith100@yahoo.com>
2024-02-03 11:39:38 +0000

    Add sorting

--- products/views.py
+++ products/views.py
@@ -14,8 +14,25 @@ def all_products(request):
     years = None
     player_issue = False
     signed = False
+    specials = None
+    sort = None
+    direction = None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'league' in request.GET:
             leagues = request.GET['league'].split(",")
             products = products.filter(league__name__in=leagues)
@@ -28,9 +45,11 @@ def all_products(request):
 
         if 'player_issue' in request.GET:
             products = products.filter(player_issue=True)
+            specials = Product.objects.filter(player_issue=True)
 
         if 'signed' in request.GET:
             products = products.filter(signed=True)
+            specials = Product.objects.filter(signed=True)
         
         if 'q' in request.GET:
             query = request.GET['q']
@@ -41,10 +60,15 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_leagues': leagues,
+        'current_years': years,
+        'current_specials': specials,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
