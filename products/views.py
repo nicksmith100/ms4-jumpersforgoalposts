026eedd55e5168f197f37ec0051c7def0e1eed77
Nick Smith <nicksmith100@yahoo.com>
2024-02-04 00:35:09 +0000

    Add sorting selector and product count

--- products/views.py
+++ products/views.py
@@ -29,7 +29,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'team':
+                sortkey = 'team__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
