e36c2197652623ed1f8c9713f47a68fbf7d083d6
Nick Smith <nicksmith100@yahoo.com>
2024-02-04 10:52:16 +0000

    Replace var with let in modal function

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    /* Event listener for flip-card function */
+    /* Flip-card functionality */
 
     $(".product-container").on("click", ".flip-card", function(e) {
         if($(e.target).is("a")) {
@@ -10,23 +10,25 @@ $(document).ready(function(){
         } else { this.classList.toggle("flipped"); }
     });
 
-
-    var imageModal = document.getElementById('image-modal')
+    /* Modal functionality */
+    
+    let imageModal = document.getElementById('image-modal')
     imageModal.addEventListener('show.bs.modal', function (event) {
-    // Anchor that triggered the modal
-    var anchor = event.relatedTarget
-    // Extract info from data-bs-* attributes
-    var image = anchor.getAttribute('data-bs-image')
-    var imageTitle = anchor.getAttribute('data-bs-image-title')
-    // If necessary, you could initiate an AJAX request here
-    // and then do the updating in a callback.
-    //
-    // Update the modal's content.
-    var modalTitle = imageModal.querySelector('.modal-title')
-    var modalBody = imageModal.querySelector('.modal-body')
+    
+        // Anchor that triggered the modal
+        let anchor = event.relatedTarget
+        
+        // Extract info from data-bs-* attributes
+        let image = anchor.getAttribute('data-bs-image')
+        let imageTitle = anchor.getAttribute('data-bs-image-title')
 
-    modalTitle.textContent = imageTitle
-    modalBody.innerHTML = `<img src="${image}" alt="${imageTitle}" class="w-100">`
+        // Update the modal's content.
+        let modalTitle = imageModal.querySelector('.modal-title')
+        let modalBody = imageModal.querySelector('.modal-body')
+
+        modalTitle.textContent = imageTitle
+        modalBody.innerHTML = `<img src="${image}" alt="${imageTitle}" class="w-100">`
+    
     })
 
 
