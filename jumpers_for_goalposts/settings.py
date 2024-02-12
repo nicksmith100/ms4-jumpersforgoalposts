4fcb35ba0f2382cd0cf1b8b1debdb3616ecd6db7
Nick Smith <nicksmith100@yahoo.com>
2024-02-12 23:13:26 +0000

    Apply changes for deployment

--- jumpers_for_goalposts/settings.py
+++ jumpers_for_goalposts/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from django.contrib.messages import constants as messages
 if os.path.exists("env.py"):
     import env
@@ -120,13 +121,18 @@ WSGI_APPLICATION = 'jumpers_for_goalposts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
 
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
