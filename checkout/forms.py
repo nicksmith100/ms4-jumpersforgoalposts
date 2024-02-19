e6289571a29764086aba4df4baf5e60bad972af0
Nick Smith <nicksmith100@yahoo.com>
2024-02-19 00:57:13 +0000

    Adjust styling of checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -35,5 +35,5 @@ class OrderForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input form-control form-control-sm'
+            self.fields[field].widget.attrs['class'] = 'form-control my-2 checkout-form-input'
             self.fields[field].label = False
