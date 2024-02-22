70d2dc4c46bee3b218d8bfc72abfc6f034ebdcf5
Nick Smith <nicksmith100@yahoo.com>
2024-02-22 09:11:49 +0000

    Add custom-form-input class to profile form

--- profiles/forms.py
+++ profiles/forms.py
@@ -30,5 +30,5 @@ class UserProfileForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'form-control my-2 profile-form-input'
+            self.fields[field].widget.attrs['class'] = 'form-control my-2 custom-form-input'
             self.fields[field].label = False
