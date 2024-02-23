4cc3051f77eb64a0dc9dd19a57c5c0b1bd5b2d23
Nick Smith <nicksmith100@yahoo.com>
2024-02-23 13:20:23 +0000

    Set debug to false

--- jumpers_for_goalposts/settings.py
+++ jumpers_for_goalposts/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 LOCAL_HOST = os.environ.get('LOCAL_HOST')
 ALLOWED_HOSTS = [LOCAL_HOST, 'jumpers-for-goalposts-13c54c4e6e2a.herokuapp.com']
