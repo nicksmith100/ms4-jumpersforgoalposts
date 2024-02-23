83527a2b7cdcb36a52b9726bc391fee16dd4e54d
Nick Smith <nicksmith100@yahoo.com>
2024-02-23 00:05:09 +0000

    Make question required field in FAQs model

--- faqs/models.py
+++ faqs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Faq(models.Model):
 
-    question = models.CharField(max_length=254, null=True, blank=True)
+    question = models.CharField(max_length=254)
     answer = models.TextField()
 
     def __str__(self):
