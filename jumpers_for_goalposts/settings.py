2c8ab49803ef575d501a529c1562b6eb6f5c1898
Nick Smith <nicksmith100@yahoo.com>
2024-02-23 23:20:42 +0000

    Update email settings

--- jumpers_for_goalposts/settings.py
+++ jumpers_for_goalposts/settings.py
@@ -235,6 +235,6 @@ else:
     EMAIL_HOST = 'smtp.mail.yahoo.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
 
 
