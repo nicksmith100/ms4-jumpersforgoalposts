aa6316663242ebf02f741ebf25427f565026b3f2
Nick Smith <nicksmith100@yahoo.com>
2024-02-24 18:11:30 +0000

    Fix protect order history to remove checkout bug

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
@@ -33,14 +33,25 @@ def profile(request):
 
 @login_required
 def order_history(request, order_number):
-     order = get_object_or_404(Order, order_number=order_number)
-     messages.info(request, (
-         f'This is a past confirmation for order number {order_number}. '
-         'A confirmation email was sent on the order date.'
-     ))
-     template = 'checkout/checkout_success.html'
-     context = {
-         'order': order,
-         'from_profile': True,
-     }
-     return render(request, template, context)
+    order = get_object_or_404(Order, order_number=order_number)
+    profile = get_object_or_404(UserProfile, user=request.user)
+    all_orders = profile.orders.all()
+    
+    if order in all_orders:
+
+        messages.info(request, (
+            f'This is a past confirmation for order number {order_number}. '
+            'A confirmation email was sent on the order date.'
+        ))
+        template = 'checkout/checkout_success.html'
+        context = {
+            'order': order,
+            'from_profile': True,
+        }
+        return render(request, template, context)
+
+    else:
+        messages.error(request, ('That order does not belong to you. Here is your profile.'))
+        return redirect(reverse('profile'))
+
+    
