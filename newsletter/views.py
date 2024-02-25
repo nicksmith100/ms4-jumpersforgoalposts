174de3ff514188e3366dd30f90f8676c4524854d
Nick Smith <nicksmith100@yahoo.com>
2024-02-25 08:38:18 +0000

    Send confirmation email on newsletter signup

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .forms import NewsletterForm
 from profiles.models import UserProfile
@@ -11,7 +14,16 @@ def newsletter(request):
         form = NewsletterForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully subscribed!')
+            form_email = form.cleaned_data['email']
+            print(form_email)
+            messages.success(request, f'Successfully subscribed! A confirmation email has been sent to {form_email}')
+            send_mail(
+                "Newsletter signup confirmation",
+                render_to_string('newsletter/emails/confirmation.txt'),
+                settings.DEFAULT_FROM_EMAIL,
+                [form_email],
+                fail_silently=False,
+            )
             return redirect(reverse('products'))
         else:
             messages.error(request, 'Failed to subscribe. Please ensure the form is valid.')
